9776206119af687ad77dbacccd8bd5eddd07681b
Juan Pablo Beltran Flores <beltranflores.juan@gmail.com>
2024-05-04 15:27:35 -0500

    Changed social links placeholders to same page

--- src/lib/constants/social.ts
+++ src/lib/constants/social.ts
@@ -4,17 +4,17 @@ const socialMedia: ISocial[] = [
 	{
 		icon: 'linkedin',
 		name: 'LinkedIn',
-		linkTo: 'www.google.com',
+		linkTo: '/',
 	},
 	{
 		icon: 'instagram',
 		name: 'Instragram',
-		linkTo: 'www.google.com',
+		linkTo: '/',
 	},
 	{
 		icon: 'facebook',
 		name: 'Facebook',
-		linkTo: 'www.google.com',
+		linkTo: '/',
 	},
 ];
 
